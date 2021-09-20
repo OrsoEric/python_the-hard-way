04f498169b56c389c23b2cbad1438591d79e1ab9
Orso <>
2021-09-20 14:04:02 +0200

    test sklearn.

--- lesson_u_machine_learning.py
+++ lesson_u_machine_learning.py
@@ -2,12 +2,16 @@
 #   sigmoid usually is able to separate probabilistically a set.
 #   e.g. obesity. some light weight are obese (dwarf) some high weight are not obese (strong man)
 
-#loss function (y-yT) output of model minus expected output
-#split data into training (80% to 90%) validation (20% to 10%)
-#durint training minimize loss on training, then see loss on validation
-#choose structure of the model: e.g. forest classificator
+##  loss function (y-yT) output of model minus expected output
+#   split data into training (80% to 90%) validation (20% to 10%)
+#   durint training minimize loss on training, then see loss on validation
+#   choose structure of the model: e.g. forest classificator
 
-#   supervised: i know what the right answer should be (usually classification and regression)
+##  overfitting
+#   when loss of train is lowering and loss of validation is increasing this means overfitting
+#   the model stopped learning the law and started memorizing the values
+
+##   supervised: i know what the right answer should be (usually classification and regression)
 #   unsupervised (clustering)
 #   semi supervised
 
@@ -82,7 +86,74 @@
 from float_to_eng_string import float_to_eng_string
 from time import perf_counter
 
+##  sklearn modules:
+#   transformer : preprocess data (not the same name of a ML model)
+#       .fit()              : calculate parameters ans saves them on the object
+#       .transform()        : use the parameters to transform the data
+#       .fit_transform()    :
+#   models      : make predictions
+#       all of above
+#       .fit                : train
+#       .predict()          : make predictions
+#       .fit_predict()      :
+#   pipeline    : i can line the transform and models boxes and from outside they act as a single model
+#       the last box must be a transformer
+#
 timestamp = perf_counter()
-import sklearn
+from sklearn.decomposition import PCA
 print(f'took {float_to_eng_string(perf_counter() -timestamp)}s to import sklearn')
 
+import pandas as pd
+
+import matplotlib.pyplot as plt
+
+import os
+
+import logging
+
+## open the CSV file
+#
+def open_csv():
+    CONFIDENTIAL_DATA_FOLDER = 'confidential_datasets'
+    CONFIDENTIAL_DATA_FILE = 'tset_H10_slip_noslip.csv'
+
+    file_name = os.path.join(os.getcwd(), CONFIDENTIAL_DATA_FOLDER, CONFIDENTIAL_DATA_FILE)
+    my_csv = pd.read_csv(file_name, header=None, sep=',')
+    return my_csv
+
+##Preprocess the datafrae
+def extract_matrix( source_csv ):
+    
+    #exclude first row
+    #exclude the last two columns
+    xxx=source_csv.iloc[1:,:-2].values.astype(float)
+    #print(f"my_csv{xxx}")
+    return xxx
+
+
+##
+def main():
+    #exercise2_correlation()
+    my_csv = open_csv()
+    #logging.info(f"my_csv{my_csv}")
+    #print(my_csv)
+
+    my_matrix = extract_matrix(my_csv)
+    print(f"data{my_matrix}, {type(my_matrix)}, {my_matrix.size}, {my_matrix.shape}")
+    data_pca = PCA(n_components=2).fit_transform(my_matrix)
+    #data_pca.plot()
+    print(f"data{data_pca}, {type(data_pca)}, {data_pca.size}, {data_pca.shape}")
+    plt.plot(data_pca)
+    plt.show()
+
+    pass
+
+## if the file is being read WITH the intent of being executed
+if __name__ == '__main__':
+    logging.basicConfig(
+        #level of debug to show
+        level=logging.DEBUG,
+        #header of the debug message
+        format='[%(asctime)s] %(levelname)s: %(message)s',
+    )
+    main()
